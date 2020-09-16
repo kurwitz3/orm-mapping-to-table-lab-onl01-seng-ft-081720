256f55fc0fe71788095ae0bba0ed42f1fd2469c7
Richard Kurwitz <kurwitz3@aol.com>
2020-09-16 21:45:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 class Student
+ 
  attr_accessor :name, :grade
  attr_reader :id 
  
@@ -15,21 +16,24 @@ class Student
    name TEXT,
    grade INTEEGER);
    SQL
+   
    DB[:conn].execute(sql) 
  end 
  
  def self.drop_table 
    sql = <<-SQL
  DROP TABLE students;
- SQL
- DB[:conn].execute(sql) 
+   SQL
+ 
+  DB[:conn].execute(sql) 
  end
    
    def save 
      sql = <<-SQL
-     INSERT INTO students (name,grade)
-     VALUES (?,?)
+      INSERT INTO students (name,grade)
+      VALUES (?,?)
      SQL
-DB[:conn].execute(sql)  
-end
+   
+    DB[:conn].execute(sql)  
+ end
 end
